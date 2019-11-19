ce9d98a13c0a3b1b1a630a34c50a15a9833d4363
peng00bo00 <pengbo_tongji@126.com>
2019-11-18 23:48:19 -0500

    add files

---
+++ 04 Perception/assignments/RoboticsPerceptionWeek3Code/ar_cube.m
@@ -0,0 +1,35 @@
+function [proj_points, t, R] = ar_cube(H,render_points,K)
+%% ar_cube
+% Estimate your position and orientation with respect to a set of 4 points on the ground
+% Inputs:
+%    H - the computed homography from the corners in the image
+%    render_points - size (N x 3) matrix of world points to project
+%    K - size (3 x 3) calibration matrix for the camera
+% Outputs: 
+%    proj_points - size (N x 2) matrix of the projected points in pixel
+%      coordinates
+%    t - size (3 x 1) vector of the translation of the transformation
+%    R - size (3 x 3) matrix of the rotation of the transformation
+% Written by Stephen Phillips for the Coursera Robotics:Perception course
+
+% YOUR CODE HERE: Extract the pose from the homography
+if H(3,3) < 0
+    H = -H;
+end
+
+h1 = H(:,1);
+h2 = H(:,2);
+h3 = cross(h1, h2);
+[U,S,V] = svd([h1 h2 h3]);
+
+R = U * [1 0 0; 0 1 0; 0 0 det(U*V')]*V';
+t = H(:,3)/norm(h1);
+P = K * [R t];
+% YOUR CODE HERE: Project the points using the pose
+N = size(render_points, 1);
+points = [render_points ones(N, 1)];
+proj_points = points * P';
+proj_points = proj_points ./ proj_points(:, 3);
+proj_points = proj_points(:, 1:2);
+
+end
--- 04 Perception/assignments/RoboticsPerceptionWeek3Code/est_homography.m
+++ 04 Perception/assignments/RoboticsPerceptionWeek3Code/est_homography.m
@@ -9,18 +9,25 @@ function [ H ] = est_homography(video_pts, logo_pts)
 % Written for the University of Pennsylvania's Robotics:Perception course
 
 % YOUR CODE HERE
-A = zeros(8, 9);
+[N, M] = size(video_pts);
+A = zeros(N*2, 9);
 
-for i=1:4
+for i=1:N
     x1  = video_pts(i, 1);
     x2  = video_pts(i, 2);
     
     x1p = logo_pts(i, 1);
     x2p = logo_pts(i, 2);
     
-    A(2*1-1, :) = [-x1, -x2, -1, 0, 0, 0, x1*x1p, x2*x1p, x1p];
-    A(2*1  , :) = ;
+    A(2*i-1, :) = [-x1, -x2, -1,   0,   0,  0, x1*x1p, x2*x1p, x1p];
+    A(2*i  , :) = [  0,   0,  0, -x1, -x2, -1, x1*x2p, x2*x2p, x2p];
 end
+
+[U, S, V] = svd(A);
+h = V(:, 9);
+
+H = reshape(h, 3, 3);
+H = H';
 
 end
 
